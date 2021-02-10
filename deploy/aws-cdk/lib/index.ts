60f86321267254bd50919db79d614735758874a4
Marcin Kopa <marcin.info@gmail.com>
2021-02-10 16:25:56 +0100

    Update dependencies

--- deploy/aws-cdk/lib/index.ts
+++ deploy/aws-cdk/lib/index.ts
@@ -1,11 +1,10 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
 
 export interface AwsCdkProps {
   // Define construct properties here
 }
 
 export class AwsCdk extends cdk.Construct {
-
   constructor(scope: cdk.Construct, id: string, props: AwsCdkProps = {}) {
     super(scope, id);
 
--- deploy/aws-cdk/test/aws-cdk.test.ts
+++ deploy/aws-cdk/test/aws-cdk.test.ts
@@ -1,15 +1,15 @@
-import { expect as expectCDK, countResources } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as AwsCdk from '../lib/index';
+import { expect as expectCDK, countResources } from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import * as AwsCdk from "../lib/index";
 
 /*
- * Example test 
+ * Example test
  */
-test('SNS Topic Created', () => {
+test("SNS Topic Created", () => {
   const app = new cdk.App();
   const stack = new cdk.Stack(app, "TestStack");
   // WHEN
-  new AwsCdk.AwsCdk(stack, 'MyTestConstruct');
+  new AwsCdk.AwsCdk(stack, "MyTestConstruct");
   // THEN
-  expectCDK(stack).to(countResources("AWS::SNS::Topic",0));
+  expectCDK(stack).to(countResources("AWS::SNS::Topic", 0));
 });
